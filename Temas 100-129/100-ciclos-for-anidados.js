94e1e8326e780531f5fba538d1bc244642895ece
Javier Alejandro Arteaga Gonzales <32112786+javierstamina@users.noreply.github.com>
2023-03-26 21:04:58 -0500

    Update 100-ciclos-for-anidados.js

--- Temas 100-129/100-ciclos-for-anidados.js
+++ Temas 100-129/100-ciclos-for-anidados.js
@@ -7,10 +7,10 @@ Tema: Ciclos "for" anidados.
 var miArreglo = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
 // Por cada arreglo anidado
-for (var i = 0; i < arreglo.length; i++) {
+for (var i = 0; i < miArreglo.length; i++) {
   var arregloAnidado = miArreglo[i];
   // Por cada elemento del arreglo anidado
-  for (var j = 0; j < fila.length; j++) {
+  for (var j = 0; j < arregloAnidado.length; j++) {
     console.log(arregloAnidado[j]);
   }
 }
